80b12de683bacfe150c0b20f93ce77213a01b176
nguyen-anh-duy <85326086+nguyen-anh-duy@users.noreply.github.com>
2021-06-04 20:00:53 +0700

    Add files via upload

---
+++ Session 1/index.js
@@ -0,0 +1,45 @@
+// Q 3
+let message = 'Coding is great';
+console.log(message)
+
+let studentCount = 0;
+console.log(studentCount);
+
+// Q 4
+message = ' Coding might noi easy, but still great';
+console.log(message)
+
+studentCount = 16;
+console.log(studentCount);
+
+let lowercaseMessage = message.toLowerCase();
+console.log(lowercaseMessage);
+
+studentCount = studentCount + 1;
+console.log(studentCount);
+
+// Q5
+let say = 'you look beautiful today';
+console.log(say);
+//alert(say);
+
+// Q6
+let Name = prompt('hi there, your name please?');
+console.log('Hi '+ Name);
+
+// Q7
+let firstName = prompt('enter your first name?');
+let lastName = prompt('enter your last name?');
+console.log('Hi '+ lastName + ' ' + firstName);
+
+// Q8
+let a = prompt('enter side leght of square?');
+console.log(Math.pow(a,2));
+
+// Q9
+let b = prompt('enter radius of the circle?');
+console.log('the circle area is ' + Math.PI * Math.pow(b,2));
+
+// Q10
+let celsius = prompt('enter temperature in Celsius?');
+console.log('Celsius ' + 'C ' + ' = ' + celsius*9/5 + ' F');
