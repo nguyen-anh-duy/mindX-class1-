2138d7cdf9f021c301beacb1214cb0f1b3b36ab8
nguyen-anh-duy <85326086+nguyen-anh-duy@users.noreply.github.com>
2021-06-07 09:57:04 +0700

    Add files via upload

---
+++ Session 4/index.js
@@ -0,0 +1,37 @@
+// học trong lớp
+// let person = ['Nam', 19 , 'Hanoi', 25 , false]
+// console.log(person);
+
+// let movie = {
+//     name: 'kong',
+//     age: 18,
+//     year: 2018
+// };
+
+// console.log(movie['name']);
+// movie['name'] = 'tom & jerry';
+// console.log(movie['name']);
+// movie['age']++
+// console.log(movie['age']);
+// movie['name1'] = 'ABC'
+// console.log(movie);
+// delete movie['name1']
+// console.log(movie);
+// movie.name = 'godzilla';
+
+// console.log(movie);
+// console.log(movie.year);
+// console.log(movie['name']);
+// console.log(movie['a']);
+
+// let person1 = {
+//     name: 'Nam',
+//     age: 18,
+//     favs: ['movie', 'cafe','code'],
+// };
+// let favs = person1['favs'];
+// for(let i = 0;i < favs.length; i = i +1 ){
+//     console.log(favs[i]);
+// }
+// favs.push('game');
+// console.log(person1);
---
+++ Session 4/question.js
@@ -0,0 +1,133 @@
+// Q1
+// const product = [{
+//     name:'Xiaomi rice cooker',
+//     price: 1700,
+//     brand: 'Xiaomi',
+//     color: 'white',
+// },
+// {name:'samsung phone',
+// price: 1800,
+// brand: 'samsung',
+// color: 'blue',
+
+// }];
+// for(let i=0; i<product.length; i=i+1){
+// console.log('name: '+ product[i]['name']);
+// console.log('price: '+ product[i]['price']);
+// console.log('brand: '+ product[i]['brand']);
+// console.log('color: '+ product[i]['color']);
+// };
+
+// // Q3
+// const task = {
+// subject : 'implement login feature',
+// createBy : 'Hoang Ngoc Duc',
+// asignTo : 'Nguyen Phuong Nam',
+// dueDate: '2019-10-08T18:00:24+0000',
+// expectedHours: 0.5,
+// };
+// console.log(typeof(task));
+// // // 3.2 ko hieu
+// // // 3.3 ko hieu
+
+// // Q4 không chạy báo lỗi
+
+// let Dictionary = {
+//  debug: 'The process of figuring out why your program has a certain error and how to fix it',
+//  done: 'When your task is complete, the only thing you have to do is to wait for users to use it (no additional codes or actions needed)'
+//  defect: 'The formal word for ‘error’',
+//  pm : 'The short version  of Project Manager, the person in charge of the final result of a project',
+//  ui/ux: 'UI means User Interface, UX mean User Experience, are the process to define how your products looks and feels',
+// };
+// let keyWord = prompt('enter key word?');
+// console.log(Dictionary[keyWord]);
+
+// Q5
+
+// 5.1
+// let item = [{
+//     name: 'Xiaomi portable charger 20000mah',
+//     brand: 'Xiaomi',
+//     price: '428' ,
+//     color: 'White',
+//     category: 'Charger'
+// },
+// {
+//     name: 'VSmart Active 1',
+//     brand: 'VSmart',
+//     price: '5487' ,
+//     color: 'Black',
+//     category: 'Phone'
+// },
+// {
+//     name: 'Iphone X',
+//     brand: 'Apple',
+//     price: '2149' ,
+//     color: 'Gray',
+//     category: 'Phone'  
+// },
+// {
+//     name: 'Samsung Galaxy A9',
+//     brand: 'Samsung',
+//     price: '8490' ,
+//     color: 'Blue',
+//     category: 'Phone'
+// }];
+// for(let i=0; i<item.length; i=i+1){
+//     console.log('#'+ Number(i+1) + ' name: '+ item[i]['name']);
+//     console.log('price: '+ item[i]['price']);
+//     console.log('................')
+// };
+
+// 5.2
+// let position = prompt('enter number position?');
+// console.log('name: '+ item[position]['name']);
+// console.log('brand: '+ item[position]['brand']);
+// console.log('price: '+ item[position]['price']);
+// console.log('color: '+ item[position]['color']);
+// console.log('category: '+ item[position]['category']);
+
+// // 5.3 bí đường làm ko ra
+// let category = prompt('enter category?');
+// switch(category){
+//     case category=='Charger';
+//     console.log
+// }
+
+// 5.4
+// item[0]['provider'] = ['Phukienzero', 'Dientuccc'];
+// item[1]['provider'] = ['Tgdd', 'Ddghn', 'VhStore'];
+// item[2]['provider'] = ['Tgdd'];
+// item[3]['provider'] = ['Tgdd'];
+// for(let i=0; i<item.length; i=i+1){
+//     console.log('#'+ Number(i+1) + ' name: '+ item[i]['name']);
+//     console.log('price: '+ item[i]['price']);
+//     console.log('provider: '+ item[i]['provider']);
+// };
+
+//5.5 bí đường như câu 5.3
+
+// Q6
+// 6.1
+// console.log('Hi there, this is your learning task to front-end developer career: ');
+// let learnJava = {
+//     Html: '',
+//     Css: '',
+//     BasicOfJavaScript: '',
+//     NodePackageManager: '',
+//     Git: '',
+// }
+// console.log(learnJava);
+// // 6.2
+// let newTask = prompt('enter new task?');
+// let newValue = prompt('enter new value for new task?');
+// learnJava[newTask] = newValue;
+// console.log(learnJava);
+
+// 6.3 bí đường ko làm ra
+
+// 6.4 bí luôn
+
+// 6.5 bí luôn
+
+// 6.6 bí luôn
