652fa6fc10dc30227080ff6731ee840b4f4ff215
nguyen-anh-duy <85326086+nguyen-anh-duy@users.noreply.github.com>
2021-06-17 11:37:31 +0700

    Add files via upload

---
+++ Session 7/index.js
@@ -0,0 +1,29 @@
+// chua thuc hien cau lenh nay
+// dung ham luu hanh dong can lam
+// phai co cap ngoac tron thi moi thuc hien cau lenh
+// function read(){
+//     alert("abcde");
+// }
+// goi ham 
+// read();
+// return tra lai gia tri cua bien ra ngoai
+// khi gap return se dung ham ko chay nua
+
+// let a = 2
+// let b = 3
+// function nhan(a,b){
+//    let c = a*b;
+//    return c;
+// }
+// let ketqua = nhan(a,b);
+// console.log(ketqua);
+
+// let input1 = prompt("enter number")
+// function giaiThua(input1){
+
+// }
+
+let great = () => {
+    console.log("abc");
+};
+setTimeout(great,5000); // doi 5s callback
---
+++ Session 7/question.js
@@ -0,0 +1,54 @@
+// Q1
+// const container = document.getElementById('list');
+// for(let i = 0; i < 3; i++) {
+//     container.insertAdjacentHTML('afterbegin', `<li>${i}</li>`)
+// };
+
+// for(let i = 0; i < 3; i++) {
+//     container.insertAdjacentHTML('afterend', `<li>${i}</li>`)
+// };
+
+// for(let i = 0; i < 3; i++) {
+//     container.insertAdjacentHTML('beforebegin', `<li>${i}</li>`)
+// };
+
+// for(let i = 0; i < 3; i++) {
+//     container.insertAdjacentHTML('beforeend', `<li>${i}</li>`)
+// };
+
+// Q2  
+// setInterval(() => {
+//     console.log('abc');
+// }, 3000);
+
+// Q3
+
+// Q4
+// let tagName = document.getElementsByTagName("li");
+// for(let i = 0; i < tagName.length; i = i +1){
+//     console.log(tagName[i].innerHTML);
+// }
+
+// Q5
+// let tagName1 = document.getElementsByTagName("div");
+// for(let i = 0; i < tagName1.length; i = i +1){
+//     console.log(tagName1[i].innerHTML);
+// }
+
+// let className = document.getElementsByClassName("singer");
+// function printClassName(){    
+//     for(let i = 0; i < className.length; i = i +1){
+//         console.log(className[i].innerHTML);
+//     }}
+
+// Q6
+// let xoa = document.getElementById("demo");
+// function myFunction(){
+//     xoa.remove();
+// };
+
+// Q12
+let item = ['Backpack', 'MiBand watch', 'Ring'];
+console.log(item);
+let ulItem = document.getElementById('items');
+console.log(ulItem);
