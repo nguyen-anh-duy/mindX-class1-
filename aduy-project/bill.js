9a5a3b131fa0d5b6fb40c5197aa550395f6ade24
nguyen-anh-duy <85326086+nguyen-anh-duy@users.noreply.github.com>
2021-07-05 16:40:23 +0700

    Add files via upload

---
+++ aduy-project/bill.js
@@ -0,0 +1,15 @@
+let inputNumber = document.getElementById("inputNumber");
+let buttonCong = document.getElementById("buttonCong");
+let buttonTru = document.getElementById("buttonTru");
+buttonCong.addEventListener("click",cong);
+buttonTru.addEventListener("click",tru);
+function cong(x){
+    buttonCong.value = 1;
+    x = Number(inputNumber.value) + Number(buttonCong.value);
+    inputNumber.value = x;
+}
+function tru(x){
+    buttonTru.value = 1;
+    x = Number(inputNumber.value) - Number(buttonCong.value);
+    inputNumber.value = x;
+}
